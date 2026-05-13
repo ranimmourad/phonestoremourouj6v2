1a04dcf1507f0323aafb3fbbbf899c04ec06938a
Ranim Mourad <ranim03mourad@gmail.com>
2026-05-13 18:01:24 +0100

    Configure for Vercel Edge Functions with Hono adapter

--- api/index.ts
+++ api/index.ts
@@ -1,30 +1,4 @@
-import type { IncomingMessage, ServerResponse } from 'http'
+import { handle } from 'hono/vercel'
 import app from '../src/index'
 
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
-  const url = `${req.headers['x-forwarded-proto'] || 'http'}://${req.headers['x-forwarded-host'] || req.headers.host}${req.url}`
-  
-  let body = ''
-  if (req.method !== 'GET' && req.method !== 'HEAD') {
-    body = await new Promise((resolve) => {
-      let data = ''
-      req.on('data', chunk => data += chunk)
-      req.on('end', () => resolve(data))
-    })
-  }
-
-  const response = await app.fetch(new Request(url, {
-    method: req.method,
-    headers: req.headers as any,
-    body: body || undefined
-  }))
-
-  const responseBody = await response.text()
-  
-  response.headers.forEach((value, key) => {
-    res.setHeader(key, value)
-  })
-  
-  res.statusCode = response.status
-  res.end(responseBody)
-}
+export default handle(app)
